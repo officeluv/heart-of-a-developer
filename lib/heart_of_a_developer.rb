65e506a7f170a90e2a5dd96266410926f4cc5a8b
bestest-mensch <16200338+bestest-mensch@users.noreply.github.com>
2019-10-04 11:05:24 -0500

    update method based on review feedback

--- lib/heart_of_a_developer.rb
+++ lib/heart_of_a_developer.rb
@@ -37,8 +37,7 @@ class HeartOfADeveloper
   ].freeze
   LINE_LENGTHS = [25, 23, 19, 15].freeze
 
-  def self.speak(user_phrase = nil)
-    phrase_to_output = user_phrase || sample_phrase
+  def self.speak(phrase_to_output = sample_phrase)
     puts illustration(partition_phrase(phrase_to_output))
   end
 
